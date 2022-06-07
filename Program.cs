ad1ef128e15b285ad2855b8a7222f12ee47b4e19
RogerLedesma <Roggerfly@gmail.com>
2022-06-07 19:09:31 -0300

    ConstructoresSobrecargayClases

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Telefono t1 = new Telefono( "iphone 12", "Apple");
+            Telefono t2= new Telefono("Samsung" , "dns");
+           
+            Console.WriteLine("La marca del telefono es : " + t1.Marca);
+            Console.WriteLine("El modelo nuevo es : " + t1.Modelo);
+
+            Console.WriteLine("MARQUE EL CODIGO DE OPERADOR PARA INGRESAR (1,2,3)");
+            t1.CodigoOperador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t1.Llamar());
+            Console.WriteLine(t1.Llamar("DIOS"));
+
+            Console.WriteLine("¿Desea probar otra marca de teléfono? Escriba la palabra samsung o omotorola..");
+            t2.Modelo2 = Console.ReadLine();
+            Console.WriteLine("Usted puede acceder a los nuevos teléfos de 5G de : " + t2.Modelo2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Telefono
+    {
+        // Constructor
+
+        public Telefono(string modelo, string marca) 
+        {
+            this.modelo = modelo;
+            this.marca = marca; 
+        }
+
+
+        // Atributos / Miembros SAMSUNG
+
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+
+       // Propiedades MOTOROLA
+       
+        public string Modelo2
+        {
+            get { return modelo; }
+            set {
+
+                if (value == "motorola")
+                    {
+                        modelo = value;
+                    } else
+
+                    do
+                        {
+                          Console.WriteLine("NO TENEMOS EN STOCK DE ESA MARCA");
+                          Console.WriteLine("Desea comprar otro telefono de otroa marca");
+                          Modelo2 = Console.ReadLine();
+
+                    } while (value == "motorola");
+
+                         
+                 }
+        }
+
+
+
+       // Propiedades SAMSUNG
+
+        public string Modelo
+        {
+          get {return modelo;}
+        }
+        
+
+        public string Marca
+        {
+          get { return marca;}
+        }
+
+        public string NumeroTelefonico
+        {
+          get {return numeroTelefonico;}
+          set {numeroTelefonico = value;}
+        }
+
+        public int CodigoOperador
+        {
+            get {return codigoOperador;}
+            set {
+
+                if (value > 0 && value <= 3)
+                {
+
+                    Console.WriteLine(">> CODIDO CORRECTO >>");
+                    codigoOperador = value;
+
+                } else
+                        
+                  do
+                    {
+                        Console.WriteLine("ERROR");
+
+                        Console.WriteLine("MARQUE EL CODIGO DE OPERADOR (1,2,3) ADENTRO");
+
+                        CodigoOperador = int.Parse(Console.ReadLine());
+
+                    } while (value > 0 && value <= 3);
+                   
+                     codigoOperador = value;
+                }
+        }
+
+        public string Llamar ()
+        {
+            return ">> Realizando llamada >>";
+        }
+
+        public string Llamar(string contacto)
+        {
+            return ">> Conectando con " + contacto + "...";
+        }
+    }
+}
